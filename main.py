e493e4ef333cf86c05799d1c002e3b1b9053dc68
Safety <safety_03@mail.ru>
2022-12-13 01:43:36 +0500

    initial

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 from telegram.update import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-import config
+import settings
 
-updater = Updater(config.BOT_TOKEN)
+updater = Updater(settings.BOT_TOKEN)
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Salom!')
-    
+
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
