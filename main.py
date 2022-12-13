f3e178c9b4dfc12772dce3e9cce501dcf257e587
Safety <safety_03@mail.ru>
2022-12-13 23:53:48 +0500

    birinchi bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from telegram.update import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
+from telegram.ext.filters import Filters
 import requests
 import settings
 
@@ -15,24 +16,31 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def search(update: Update, context: CallbackContext):
     args = context.args
-    search_text = ' '.join(args)
-    response = requests.get('https://uz.wikipedia.org/w/api.php', {
-        'action': 'opensearch',
-        'search':  search_text,
-        'limit': 1,
-        'namespace': 0,
-        'format': 'json',
-    })
-    result = response.json()
-    link = result[3]
-    if link:
-        update.message.reply_text("Sizning so'rovingiz bo'yicha ma'lumotlar: " + link[0])
+    if len(args) == 0:
+        update.message.reply_text("Biror ma'lumot kiriting...")
     else:
-        update.message.reply_text("Sizning so'rovingiz bo'yicha ma'lumotlar topilmadi!")
+        search_text = ' '.join(args)
+        response = requests.get('https://uz.wikipedia.org/w/api.php', {
+            'action': 'opensearch',
+            'search':  search_text,
+            'limit': 5,
+            'namespace': 0,
+            'format': 'json',
+        })
+        result = response.json()
+        print(result)
+        link = result[3]
+        if link:
+            update.message.reply_text("Sizning so'rovingiz bo'yicha ma'lumotlar:")
+            for lin in link:
+                update.message.reply_text(lin)
+        else:
+            update.message.reply_text("Sizning so'rovingiz bo'yicha ma'lumotlar topilmadi!")
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('search', search))
+dispatcher.add_handler(MessageHandler(Filters.all, start))
 
 updater.start_polling()
 updater.idle()
